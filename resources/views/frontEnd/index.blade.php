78671c0615d88f7dde45ffde184b9d94ca71fef6
Foysal <foysal20599@gmail.com>
2021-10-06 22:46:37 +0600

    index file and asset folder video add

--- resources/views/frontEnd/index.blade.php
+++ resources/views/frontEnd/index.blade.php
@@ -1,6 +1,38 @@
 @extends('frontEnd.layouts.master')
 @push('style')
 <style>
+    .hytPlayerWrap {
+        display: inline-block;
+        position: relative;
+    }
+    .hytPlayerWrap.ended::after {
+        content:"";
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        cursor: pointer;
+        background-color: black;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 64px 64px;
+        background-image: url(data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjgiIGhlaWdodD0iMTI4IiB2aWV3Qm94PSIwIDAgNTEwIDUxMCI+PHBhdGggZD0iTTI1NSAxMDJWMEwxMjcuNSAxMjcuNSAyNTUgMjU1VjE1M2M4NC4xNSAwIDE1MyA2OC44NSAxNTMgMTUzcy02OC44NSAxNTMtMTUzIDE1My0xNTMtNjguODUtMTUzLTE1M0g1MWMwIDExMi4yIDkxLjggMjA0IDIwNCAyMDRzMjA0LTkxLjggMjA0LTIwNC05MS44LTIwNC0yMDQtMjA0eiIgZmlsbD0iI0ZGRiIvPjwvc3ZnPg==);
+    }
+    .hytPlayerWrap.paused::after {
+        content:"";
+        position: absolute;
+        top: 70px;
+        left: 0;
+        bottom: 50px;
+        right: 0;
+        cursor: pointer;
+        background-color: black;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 40px 40px;
+        background-image: url(data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEiIHdpZHRoPSIxNzA2LjY2NyIgaGVpZ2h0PSIxNzA2LjY2NyIgdmlld0JveD0iMCAwIDEyODAgMTI4MCI+PHBhdGggZD0iTTE1Ny42MzUgMi45ODRMMTI2MC45NzkgNjQwIDE1Ny42MzUgMTI3Ny4wMTZ6IiBmaWxsPSIjZmZmIi8+PC9zdmc+);
+    }
 
 </style>
 @endpush
@@ -156,45 +188,18 @@
     <!-- home page video popup section -->
 <section class="ssitl-videohny" id="video">
     <div class="new-block py-5">
-        <div class="container py-lg-5">
-                <div class="history-info position-relative">
-                <iframe id="myAudio" style="border-radius:10px;" width="100%;" height="100%;" 
-                src="https://www.youtube.com/embed/8hXOzbGS-CI?rel=0&autoplay=1" allow="autoplay" frameborder="0" allowfullscreen></iframe>
-
-                   
-                </div>
+        <div class="container py-lg-5 hytPlayerWrapOuter">
+                <div class="history-info position-relative hytPlayerWrap">
+                    {{-- <iframe height="100%;" width="100%;" src="https://www.youtube.com/embed/8hXOzbGS-CI?autoplay=1"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> --}}
+                 
+                     <iframe loading="lazy" height="100%;" width="100%;" src="https://www.youtube.com/embed/8hXOzbGS-CI?rel=0&enablejsapi=1" frameborder="0"> </iframe>
+            </div>
     </div>
 
-   
-
-</section>
-<audio controls autoplay>
-<source src="{{ asset('public/frontEnd') }}/assets/audio/dipumoni.mp3" type="audio/mpeg">
-Your browser does not support the audio element.
-</audio>
 
 
-<audio id="myAudio" controls autoplay>
-    <source autoplay src="{{ asset('public/frontEnd') }}/assets/audio/dipumoni.mp3" type="audio/mp3">
-    Your browser does not support the audio element.
-  </audio>
-  
-  <p>Click the button to find out if the audio automatically started to play as soon as it was ready.</p>
-  
 
-  
-  <script>
-    var x = document.getElementById("myAudio");
-    x.autoplay = true;
-    x.load();
-
-  
-  </script>
-
-
-  
-
-
+    
 <!-- //home page video popup section -->
     <!--/blog-post-->
     {{-- <section class="ssitl-blogpost-content py-5">
@@ -480,9 +485,58 @@ Your browser does not support the audio element.
         <!-- //grids -->
     </section>
     <!-- //testimonials --> --}}
-    <script type="text/javascript">
-        document.getElementById("myAudio").unMute();
-      </script>
+    <script>
+        "use strict";
+        document.addEventListener('DOMContentLoaded', function() {
+            if (window.hideYTActivated) return;
+            let onYouTubeIframeAPIReadyCallbacks = [];
+            for (let playerWrap of document.querySelectorAll(".hytPlayerWrap")) {
+                let playerFrame = playerWrap.querySelector("iframe");
+    
+                let tag = document.createElement('script');
+                tag.src = "https://www.youtube.com/iframe_api";
+                let firstScriptTag = document.getElementsByTagName('script')[0];
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+                let onPlayerStateChange = function(event) {
+                    if (event.data == YT.PlayerState.ENDED) {
+                        playerWrap.classList.add("ended");
+                    } else if (event.data == YT.PlayerState.PAUSED) {
+                        playerWrap.classList.add("paused");
+                    } else if (event.data == YT.PlayerState.PLAYING) {
+                        playerWrap.classList.remove("ended");
+                        playerWrap.classList.remove("paused");
+                    }
+                };
+    
+                let player;
+                onYouTubeIframeAPIReadyCallbacks.push(function() {
+                    player = new YT.Player(playerFrame, {
+                        events: {
+                            'onStateChange': onPlayerStateChange
+                        }
+                    });
+                });
+    
+                playerWrap.addEventListener("click", function() {
+                    let playerState = player.getPlayerState();
+                    if (playerState == YT.PlayerState.ENDED) {
+                        player.seekTo(0);
+                    } else if (playerState == YT.PlayerState.PAUSED) {
+                        player.playVideo();
+                    }
+                });
+            }
+    
+            window.onYouTubeIframeAPIReady = function() {
+                for (let callback of onYouTubeIframeAPIReadyCallbacks) {
+                    callback();
+                }
+            };
+    
+            window.hideYTActivated = true;
+        });
+    </script>
 
    @include('frontEnd.include.footer')
    @endsection
